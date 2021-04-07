6eb0be15f3cdc47db6c5f9c99c2a0459aa149413
Nikhil Reddy Pannala <77602266+Nikhilreddy012@users.noreply.github.com>
2021-04-07 14:26:58 -0500

    implemented post request for newspapers

--- controllers/newspaper.js
+++ controllers/newspaper.js
@@ -13,8 +13,23 @@ exports.newspaper_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Newspaper detail: ' + req.params.id);
 };
 // Handle Newspaper create on POST.
-exports.newspaper_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Newspaper create POST');
+exports.newspaper_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Newspaper();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.name = req.body.name;
+    document.language = req.body.language;
+    document.price = req.body.price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
 };
 // Handle Newspaper delete form on DELETE.
 exports.newspaper_delete = function(req, res) {
