13f7510ec971dfcc77cea36dcde9dfdf38fa40b5
Nikhil Reddy Pannala <77602266+Nikhilreddy012@users.noreply.github.com>
2021-04-23 06:19:31 -0500

    protected the update endpoint

--- routes/newspapers.js
+++ routes/newspapers.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const newspaper_controllers=require('../controllers/newspaper');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', newspaper_controllers.newspaper_view_all_Page);
@@ -9,7 +19,7 @@ router.get('/detail', newspaper_controllers.newspaper_view_one_Page);
 /* GET create newspaper page */
 router.get('/create', newspaper_controllers.newspaper_create_Page);
 /* GET create update page */
-router.get('/update', newspaper_controllers.newspaper_update_Page);
+router.get('/update', secured, newspaper_controllers.newspaper_update_Page);
 /* GET create costume page */
 router.get('/delete', newspaper_controllers.newspaper_delete_Page);
 module.exports = router;
