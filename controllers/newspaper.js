1a741fe3508eed0e860793baef76c6056b7500b6
Nikhil Reddy Pannala <77602266+Nikhilreddy012@users.noreply.github.com>
2021-04-23 11:55:41 -0500

    Added create, detail, and delete buttons

--- controllers/newspaper.js
+++ controllers/newspaper.js
@@ -35,7 +35,8 @@ exports.newspaper_create_post = async function(req, res) {
     res.send(result);
     }
     catch(err){
-    res.error(500,`{"error": ${err}}`);
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
     }
 };
 // Handle Newspaper delete form on DELETE.
